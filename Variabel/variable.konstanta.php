8203588496583d5bf4d86e8d93d889e1853725f9
akmalr17 <91894973+akmalr17@users.noreply.github.com>
2022-02-25 20:37:29 +0700

    Add files via upload

---
+++ Variabel/variable.konstanta.php
@@ -0,0 +1,28 @@
+<?php
+
+    //definisikan konstanta
+    define('PHI', 3.14);
+    define('DBNAME', 'db_mahasiswa');
+    define('DBSERVER', 'localhost');
+    define('DBUSER', 'Root');
+    define('DBPASS', 'udin');
+    
+
+    //deklarsi variabel
+    $jari = 10;
+    $luas = PHI * $jari * $jari;
+    $keliling = 2* PHI * $jari;
+
+    //menampilkan hasil
+    echo 'Luas Lingkaran : '.$luas.'<br>';
+    echo 'Keliling Lingkaran : '.$keliling;
+
+    echo '<hr>';
+
+    //menampilkan Konstanta
+    echo 'Nama Database : '.DBNAME.'<br>';
+    echo 'Server Database : '.DBSERVER.'<br>';
+    echo 'Nama User Database : '.DBUSER.'<br>';
+    echo 'Password Database : '.DBPASS;
+
+?>
---
+++ Variabel/variable_user.php
@@ -0,0 +1,12 @@
+<?php
+
+    //deklarasi variabel
+    $nama = 'Rizki';
+    $umur = 20;
+    $berat = 60.2;
+
+    echo 'Nama saya : '. $nama;
+    echo '<br> Umur saya : '. $umur.' Tahun';
+    echo '<br> berat saya : '. $berat.' Kg';
+    
+?>
